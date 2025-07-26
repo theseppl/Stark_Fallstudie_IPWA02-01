9fa0b9b6c33c961ee9535332a9dd4725837b3dad
Sebastian Stark <sstark@gmx.net>
2025-07-26 14:54:55 +0200

    Impressum begonnen / Seitenwechsel implementiert

---
+++ Aufgabe3_GhostNetFishing/src/main/java/ghostNetFishing/ActionController.java
@@ -0,0 +1,42 @@
+package ghostNetFishing;
+
+import java.io.Serializable;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+@Named("actionController")
+@ViewScoped
+public class ActionController
+             implements Serializable
+{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+//	@Inject 
+//	private Aufgabenliste aufgabenliste;
+
+	public String impressum() {
+        return "impressum";
+    }
+ 
+/*	
+    public String stopEdit() {
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("ghostNetPersistenceUnit");
+    	
+    	EntityManager em = emf.createEntityManager();
+        EntityTransaction t = em.getTransaction();
+        t.begin();
+        for (Aufgabe a : aufgabenliste.getListe())
+          em.merge(a);
+        t.commit();
+        return "aufgabenliste";
+    }
+    */
+}
