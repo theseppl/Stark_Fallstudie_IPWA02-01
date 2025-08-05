28ec7b15133e12a4d3d09df14f2d91c6b39dbd0b
Sebastian Stark <sstark@gmx.net>
2025-08-05 20:33:10 +0200

    Speicherung in NetDB ermöglicht und Variablen umbenannt

--- Aufgabe3_GhostNetFishing/src/main/java/ghostNetFishing/Netz.java
+++ Aufgabe3_GhostNetFishing/src/main/java/ghostNetFishing/Netz.java
@@ -12,103 +12,144 @@ public class Netz {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int ID;
 	
-    private int latituteSliderDegree;
-    private int latituteSliderMinute;
-    private int latituteSliderSeconds;
-    private int longituteSliderDegree;
-    private int longituteSliderMinute;
-    private int longituteSliderSeconds;
-    private int netSizeSlider = 1;
-    private String northSouthRadio;
-    private String eastWestRadio;
+	private String northSouth;
+    private int latituteDegree;
+    private int latituteMinute;
+    private int latituteSeconds;
+    
+    private String eastWest;
+    private int longituteDegree;
+    private int longituteMinute;
+    private int longituteSeconds;
+    
+    private int netSize = 1;
     private LocalDate dateDe;
     private LocalDate today = LocalDate.now();
+    private String status = "gemeldet";
     
     public Netz() {}
     
-	public Netz(int latituteSliderDegree, int latituteSliderMinute, int latituteSliderSeconds, int longituteSliderDegree,
-			int longituteSliderMinute, int longituteSliderSeconds, int netSizeSlider, String northSouthRadio,
-			String eastWestRadio, LocalDate dateDe, LocalDate today) {
+	public Netz(int latituteDegree, int latituteMinute, int latituteSeconds, int longituteDegree,
+			int longituteMinute, int longituteSeconds, int netSize, String northSouth,
+			String eastWest, LocalDate dateDe, LocalDate today) {
 		super();
-		this.latituteSliderDegree = latituteSliderDegree;
-		this.latituteSliderMinute = latituteSliderMinute;
-		this.latituteSliderSeconds = latituteSliderSeconds;
-		this.longituteSliderDegree = longituteSliderDegree;
-		this.longituteSliderMinute = longituteSliderMinute;
-		this.longituteSliderSeconds = longituteSliderSeconds;
-		this.netSizeSlider = netSizeSlider;
-		this.northSouthRadio = northSouthRadio;
-		this.eastWestRadio = eastWestRadio;
+		this.latituteDegree = latituteDegree;
+		this.latituteMinute = latituteMinute;
+		this.latituteSeconds = latituteSeconds;
+		this.longituteDegree = longituteDegree;
+		this.longituteMinute = longituteMinute;
+		this.longituteSeconds = longituteSeconds;
+		this.netSize = netSize;
+		this.northSouth = northSouth;
+		this.eastWest = eastWest;
 		this.dateDe = dateDe;
 		this.today = today;
 	}
 
-	public int getLatituteSliderDegree() {
-		return latituteSliderDegree;
+	public int getID() {
+		return ID;
 	}
-	public void setLatituteSliderDegree(int latituteSliderDegree) {
-		this.latituteSliderDegree = latituteSliderDegree;
+
+	public void setID(int iD) {
+		ID = iD;
 	}
-	public int getLatituteSliderMinute() {
-		return latituteSliderMinute;
+
+	public int getLatituteDegree() {
+		return latituteDegree;
 	}
-	public void setLatituteSliderMinute(int latituteSliderMinute) {
-		this.latituteSliderMinute = latituteSliderMinute;
+
+	public void setLatituteDegree(int latituteDegree) {
+		this.latituteDegree = latituteDegree;
 	}
-	public int getLatituteSliderSeconds() {
-		return latituteSliderSeconds;
+
+	public int getLatituteMinute() {
+		return latituteMinute;
 	}
-	public void setLatituteSliderSeconds(int latituteSliderSeconds) {
-		this.latituteSliderSeconds = latituteSliderSeconds;
+
+	public void setLatituteMinute(int latituteMinute) {
+		this.latituteMinute = latituteMinute;
 	}
-	public int getLongituteSliderDegree() {
-		return longituteSliderDegree;
+
+	public int getLatituteSeconds() {
+		return latituteSeconds;
 	}
-	public void setLongituteSliderDegree(int longituteSliderDegree) {
-		this.longituteSliderDegree = longituteSliderDegree;
+
+	public void setLatituteSeconds(int latituteSeconds) {
+		this.latituteSeconds = latituteSeconds;
 	}
-	public int getLongituteSliderMinute() {
-		return longituteSliderMinute;
+
+	public int getLongituteDegree() {
+		return longituteDegree;
 	}
-	public void setLongituteSliderMinute(int longituteSliderMinute) {
-		this.longituteSliderMinute = longituteSliderMinute;
+
+	public void setLongituteDegree(int longituteDegree) {
+		this.longituteDegree = longituteDegree;
 	}
-	public int getLongituteSliderSeconds() {
-		return longituteSliderSeconds;
+
+	public int getLongituteMinute() {
+		return longituteMinute;
 	}
-	public void setLongituteSliderSeconds(int longituteSliderSeconds) {
-		this.longituteSliderSeconds = longituteSliderSeconds;
+
+	public void setLongituteMinute(int longituteMinute) {
+		this.longituteMinute = longituteMinute;
 	}
-	public int getNetSizeSlider() {
-		return netSizeSlider;
+
+	public int getLongituteSeconds() {
+		return longituteSeconds;
 	}
-	public void setNetSizeSlider(int netSizeSlider) {
-		this.netSizeSlider = netSizeSlider;
+
+	public void setLongituteSeconds(int longituteSeconds) {
+		this.longituteSeconds = longituteSeconds;
 	}
-	public String getNorthSouthRadio() {
-		return northSouthRadio;
+
+	public int getNetSize() {
+		return netSize;
 	}
-	public void setNorthSouthRadio(String northSouthRadio) {
-		this.northSouthRadio = northSouthRadio;
+
+	public void setNetSize(int netSize) {
+		this.netSize = netSize;
 	}
-	public String getEastWestRadio() {
-		return eastWestRadio;
+
+	public String getNorthSouth() {
+		return northSouth;
 	}
-	public void setEastWestRadio(String eastWestRadio) {
-		this.eastWestRadio = eastWestRadio;
+
+	public void setNorthSouth(String northSouth) {
+		this.northSouth = northSouth;
 	}
+
+	public String getEastWest() {
+		return eastWest;
+	}
+
+	public void setEastWest(String eastWest) {
+		this.eastWest = eastWest;
+	}
+
 	public LocalDate getDateDe() {
 		return dateDe;
 	}
+
 	public void setDateDe(LocalDate dateDe) {
 		this.dateDe = dateDe;
 	}
+
 	public LocalDate getToday() {
 		return today;
 	}
+
 	public void setToday(LocalDate today) {
 		this.today = today;
-	}  
-	
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
 }
 
